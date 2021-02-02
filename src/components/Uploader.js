3476bb5498a0b6b854494140a75e7977bae7450e
GelatoMao <gelatomao@outlook.com>
2021-02-02 23:39:41 +0800

    实现图片上传及图片信息展示

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,32 +1,80 @@
 import React, { useRef } from 'react'
-import { observer } from 'mobx-react'
+import { observer, useLocalStore } from 'mobx-react'
 import { useStores } from '../stores'
-import { Upload } from 'antd'
+import { message, Upload } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
+import styled from 'styled-components'
+
+
 const { Dragger } = Upload
 
+const ImageShow = styled.div`
+  margin-top: 30px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`
+
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+
+const Image = styled.img`
+  max-width: 300px;
+`
+
 const ComponentUploader = observer(() => {
-  const { ImageStore } = useStores()
-  const ref = useRef()
-  const props = {
-    beforeUpload: file => {
-      console.log(file)
-      return false
+
+  const { ImageStore, UserStore } = useStores()
+  const ref1 = useRef()
+  const ref2 = useRef()
+
+  const store = useLocalStore(() => ({
+    width: null,
+    setWidth(width) {
+      store.width = width
+    },
+    get widthStr() {
+      return store.width ? `/w/${store.width}` : ''
+    },
+    height: null,
+    setHeight(height) {
+      store.height = height
+    },
+    get heightStr() {
+      return store.height ? `/h/${store.height}` : ''
+    },
+    get fullStr() {
+      return ImageStore.serverFile.attributes.url.attributes.url + '?imageView2/0' + store.widthStr + store.heightStr
     }
+  }))
+
+  const bindWidthChange = () => {
+    store.setWidth(ref1.current.value)
   }
-  const bindChange = () => {
-    console.log(ref.current)
-    if (ref.current.files.length > 0) {
-      ImageStore.setFile(ref.current.files[0])
-      ImageStore.setFilename(ref.current.files[0].name)
+
+  const bindHeightChange = () => {
+    store.setHeight(ref2.current.value)
+  }
+
+  const props = {
+    // 不显示列表
+    showUploadList: false,
+    beforeUpload: file => {
+      ImageStore.setFile(file)
+      ImageStore.setFilename(file.name)
+      if (UserStore.currentUser === null) {
+        message.warning('请先登录再上传！')
+        return false
+      }
       ImageStore.upload().then((serverFile) => {
         console.log('上传成功')
         console.log(serverFile)
       }).catch(() => {
         console.log('上传失败')
       })
+      return false
     }
-    window.file = ref.current
   }
 
   return (
@@ -41,6 +89,26 @@ const ComponentUploader = observer(() => {
           band files
         </p>
       </Dragger>
+      {
+        ImageStore.serverFile ? <ImageShow>
+          <H1>上传结果</H1>
+          <dl>
+            <dt>线上地址</dt>
+            <dd><a target="_blank" href={ImageStore.serverFile.attributes.url.attributes.url}>{ImageStore.serverFile.attributes.url.attributes.url}</a></dd>
+            <dt>文件名</dt>
+            <dd>{ImageStore.filename}</dd>
+            <dt>图片预览</dt>
+            <dd><Image src={ImageStore.serverFile.attributes.url.attributes.url} alt="" /></dd>
+            <dt>更多尺寸</dt>
+            <dd>
+              <input ref={ref1} onChange={bindWidthChange} placeholder="最大宽度(可选)" />
+              <input ref={ref2} onChange={bindHeightChange} placeholder="最大高度(可选)" />
+            </dd>
+            <dd><a target="_blank" href={store.fullStr} alt="">{store.fullStr}</a></dd>
+
+          </dl>
+        </ImageShow> : null
+      }
     </div>
   )
 })
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { useStores } from '../stores'
 import Uploader from '../components/Uploader'
+import Tips from '../components/Tips'
 
 const Home = observer(() => {
   const { UserStore } = useStores()
   const User = () => <>Hello {UserStore.currentUser.attributes.username}</>
   return (
     <>
-      {
-        UserStore.currentUser ? <User /> : "用户没有登录"
-      }
+      <Tips>请先登录再上传</Tips>
       <Uploader />
     </>
   )
